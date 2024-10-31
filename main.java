131c1f1680781eef37d01c99c1977d7ad47032fa
Luana <128156531+Lluanagabriela@users.noreply.github.com>
2024-10-31 08:14:56 -0300

    .

---
+++ main.java
@@ -0,0 +1,13 @@
+class HelloWorld {
+  public static void main(String[] args) {
+   System.out.println("Esse e o Screen Match");
+   System.out.println("Filme: Oppenheimer");
+   
+   int ano = 2023;
+   
+   System.out.println("Ano de lancamento:" + ano);
+   
+   boolean incluidoNoPlano = true;
+   double notaFilme = 9.5;
+  }
+}
