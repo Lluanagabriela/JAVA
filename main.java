a1731faed1ce3f6fba0f1b45687939d0427d007e
Luana <128156531+Lluanagabriela@users.noreply.github.com>
2024-11-07 08:07:59 -0300

    .

--- main.java
+++ main.java
@@ -11,11 +11,14 @@ class HelloWorld {
 
   System.out.println("Ano de lancamento:" + ano);
    
-   double media = (8.5 + 9.0. + 9.5)/3;
+   double media = (8.5 + 9.0 + 9.5)/3;
    System.out.println("A media das notas deste filme e:" + media);
 
    String sinopse;
-   sinopse = "Oppenheimer é um filme histórico de drama dirigido por Christopher Nolan e baseado no livro biográfico vencedor do Prêmio Pulitzer, Prometeu Americano: O Triunfo e a Tragédia de J. Robert Oppenheimer, escrito por Kai Bird e Martin J. Sherwin. Ambientado na Segunda Guerra Mundial, o longa acompanha a vida de J. Robert Oppenheimer (Cillian Murphy), físico teórico da Universidade da Califórnia e diretor do Laboratório de Los Alamos durante o Projeto Manhattan - que tinha a missão de projetar e construir as primeiras bombas atômicas. A trama acompanha o físico e um grupo formado por outros cientistas ao longo do processo de desenvolvimento da arma nuclear que foi responsável pelas tragédias nas cidades de Hiroshima e Nagasaki, no Japão, em 1945.\n";
+   sinopse = "Oppenheimer é um filme histórico de drama dirigido por Christopher Nolan e baseado no livro biográfico vencedor do Prêmio Pulitzer, Prometeu Americano: O Triunfo e a Tragédia de J. Robert Oppenheimer, escrito por Kai Bird e Martin J. Sherwin. Ambientado na Segunda Guerra Mundial, o longa acompanha a vida de J. Robert Oppenheimer (Cillian Murphy), físico teórico da Universidade da Califórnia e diretor do Laboratório de Los Alamos durante o Projeto Manhattan - que tinha a missão de projetar e construir as primeiras bombas atômicas. A trama acompanha o físico e um grupo formado por outros cientistas ao longo do processo de desenvolvimento da arma nuclear que foi responsável pelas tragédias nas cidades de Hiroshima e Nagasaki, no Japão, em 1945.";
    System.out.println(sinopse);
+
+   int classificação = (int) media/2;
+   System.out.println("Estrelas: "+ classificação);
   }
 }
